cf6083b16a4065e387e2aee41b5503e591debe61
eric <ministor@126.com>
2018-10-23 12:16:12 +0800

    add grace

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/facebookgo/grace/gracehttp"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/sync/errgroup"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -53,7 +54,7 @@ func main() {
 		}
 
 		g.Go(func() error {
-			return server.ListenAndServe()
+			return gracehttp.Serve(server)
 		})
 	}
 
